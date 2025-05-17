c0aba5b95605705fd8bcdee31543f23ac9f8194b
Marco Zulian <codebymarco1@gmail.com>
2025-05-17 16:29:47 -0300

    implements GET and SET

--- app/main.go
+++ app/main.go
@@ -12,6 +12,7 @@ import (
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+var store = make(map[string]string)
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -70,6 +71,15 @@ func handleConn(conn net.Conn) {
 		case "ECHO":
 			answer := fmt.Sprintf("$%d\r\n%v\r\n", len(command[1]), command[1])
 			conn.Write([]byte(answer))
+		case "SET":
+			store[command[1]] = command[2]
+			conn.Write([]byte("+OK\r\n"))
+		case "GET":
+			if val, ok := store[command[1]]; ok {
+				conn.Write(fmt.Appendf(nil, "$%d\r\n%v\r\n", len(val), val))
+				break
+			}
+			conn.Write([]byte("$-1\r\n"))
 		default:
 			conn.Write([]byte("+PONG\r\n"))
 		}
